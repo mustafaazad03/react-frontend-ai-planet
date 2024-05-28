cd3b4027d0c8db9f14a666b6cc367550cb0369bb
Mustafa Azad <mustafaazad533@gmail.com>
2024-05-28 17:47:11 +0530

    Navbar UI implemented and Added Context file with custom hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,61 @@
+import aiplanetLogo from "../assets/logo.svg";
+import fileIcon from "../assets/file.svg";
+import { useAppContext } from "../context";
+import addIcon from "../assets/add.svg";
+import { useRef } from "react";
 const Navbar = () => {
-	return <div></div>;
+	const { pdfMetaData, uploadPdf } = useAppContext();
+	const fileInputRef = useRef(null);
+
+	const handleUploadClick = () => {
+		fileInputRef.current.click();
+	};
+
+	const handleFileChange = async (event) => {
+		const file = event.target.files[0];
+		if (file) {
+			await uploadPdf(file);
+		}
+	};
+	return (
+		<div className="shadow-nav py-[18px] flex justify-between items-center md:px-14 px-8">
+			<a href="/">
+				<img src={aiplanetLogo} alt="Aiplanet Logo" className="" />
+			</a>
+			<div className="flex items-center md:space-x-9 space-x-5">
+				{pdfMetaData?.pdf_id !== null && (
+					<div className="flex items-center justify-center">
+						<img
+							src={fileIcon}
+							alt="File Icon"
+							className="mr-2 p-2 border rounded-sm border-primary/45"
+						/>
+						<p className="text-sm text-primary font-medium">
+							{pdfMetaData.name.length > 8
+								? pdfMetaData.name.slice(0, 8) +
+								  "..." +
+								  pdfMetaData.name.slice(-4)
+								: pdfMetaData.name}
+						</p>
+					</div>
+				)}
+				<button
+					className="flex items-center space-x-3 sm:px-9 sm:py-[10px] p-[10px] border rounded-lg border-black"
+					onClick={handleUploadClick}
+				>
+					<img src={addIcon} alt="Add Icon" />
+					<span className="font-semibold hidden sm:flex">Upload PDF</span>
+				</button>
+				<input
+					type="file"
+					ref={fileInputRef}
+					className="hidden"
+					accept="application/pdf"
+					onChange={handleFileChange}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
